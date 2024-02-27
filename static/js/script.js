f1d0dabac94197a87554237cf9366bf562606def
Hailin Xie <sghxie4@liverpool.ac.uk>
2024-02-27 22:13:26 +0000

    added javascript

---
+++ static/js/script.js
@@ -0,0 +1,8 @@
+// 获取按钮元素
+const helloButton = document.getElementById('helloButton');
+
+// 添加点击事件监听器
+helloButton.addEventListener('click', function() {
+  // 显示 "Hello, world!" 消息
+  alert('Hello, world!');
+});
