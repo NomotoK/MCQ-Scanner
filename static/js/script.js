dfde0f0bf512ad1e7fa786b7b10e25d2da9b8578
Hailin Xie <sghxie4@liverpool.ac.uk>
2024-02-28 22:36:38 +0000

    updated index, style

--- static/js/script.js
+++ static/js/script.js
@@ -4,25 +4,51 @@ let userSwitch = document.getElementById('userSwitch');
 let myHeading = document.querySelector("h1");
 
 
-
 function setUserName() {
   let myName = prompt("请输入你的名字。");
   if (!myName) {
     setUserName();
   } else {
     localStorage.setItem("name", myName);
-    myHeading.textContent = "Mozilla 酷毙了，" + myName;
+    myHeading.textContent = "Welcome to my site，" + myName;
   }
 }
-
 
 
 if (!localStorage.getItem("name")) {
   setUserName();
 } else {
   let storedName = localStorage.getItem("name");
-  myHeading.textContent = "Mozilla 酷毙了，" + storedName;
+  myHeading.textContent = "Welcome to my site，" + storedName;
 }
+
+
+function previewPDF() {
+  const fileInput = document.getElementById('pdf-upload');
+  const pdfPreview = document.getElementById('pdf-preview');
+
+  if (fileInput.files.length === 0) {
+      alert("Please select a PDF file to upload.");
+      return;
+  }
+
+  const file = fileInput.files[0];
+  if(file.type !== "application/pdf") {
+      alert("Please select a PDF file.");
+      return;
+  }
+
+  // Use FileReader to read file
+  const fileReader = new FileReader();
+
+  fileReader.onload = function() {
+      pdfPreview.src = fileReader.result;
+      pdfPreview.hidden = false;
+  };
+
+  fileReader.readAsDataURL(file);
+}
+
 
 
 // 添加点击事件监听器
