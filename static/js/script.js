e6c154870b87987b05466473b201a75029f3dc9f
Hailin Xie <sghxie4@liverpool.ac.uk>
2024-02-28 22:59:02 +0000

    reconstructed webpage

--- static/js/script.js
+++ static/js/script.js
@@ -1,26 +1,25 @@
 // 获取按钮元素
-let helloButton = document.getElementById('helloButton');
-let userSwitch = document.getElementById('userSwitch');
-let myHeading = document.querySelector("h1");
+
+// let myHeading = document.querySelector("h1");
 
 
-function setUserName() {
-  let myName = prompt("请输入你的名字。");
-  if (!myName) {
-    setUserName();
-  } else {
-    localStorage.setItem("name", myName);
-    myHeading.textContent = "Welcome to my site，" + myName;
-  }
-}
+// function setUserName() {
+//   let myName = prompt("请输入你的名字。");
+//   if (!myName) {
+//     setUserName();
+//   } else {
+//     localStorage.setItem("name", myName);
+//     myHeading.textContent = "Welcome to my site，" + myName;
+//   }
+// }
 
 
-if (!localStorage.getItem("name")) {
-  setUserName();
-} else {
-  let storedName = localStorage.getItem("name");
-  myHeading.textContent = "Welcome to my site，" + storedName;
-}
+// if (!localStorage.getItem("name")) {
+//   setUserName();
+// } else {
+//   let storedName = localStorage.getItem("name");
+//   myHeading.textContent = "Welcome to my site，" + storedName;
+// }
 
 
 function previewPDF() {
@@ -49,14 +48,3 @@ function previewPDF() {
   fileReader.readAsDataURL(file);
 }
 
-
-
-// 添加点击事件监听器
-helloButton.addEventListener('click', function() {
-  // 显示 "Hello, world!" 消息
-  alert('Hello, world!');
-});
-
-userSwitch.addEventListener('click', function() {
-  setUserName();
-});
