7b78084b85eb6fd781a40c29161c36bad7051466
Hailin Xie <sghxie4@gmail.com>
2024-03-02 12:18:41 +0000

    updated UI

--- static/js/script.js
+++ static/js/script.js
@@ -1,26 +1,3 @@
-// 获取按钮元素
-
-// let myHeading = document.querySelector("h1");
-
-
-// function setUserName() {
-//   let myName = prompt("请输入你的名字。");
-//   if (!myName) {
-//     setUserName();
-//   } else {
-//     localStorage.setItem("name", myName);
-//     myHeading.textContent = "Welcome to my site，" + myName;
-//   }
-// }
-
-
-// if (!localStorage.getItem("name")) {
-//   setUserName();
-// } else {
-//   let storedName = localStorage.getItem("name");
-//   myHeading.textContent = "Welcome to my site，" + storedName;
-// }
-
 
 function previewPDF() {
   const fileInput = document.getElementById('pdf-upload');
