11b8e13008ef3388eeabb9c1df5fa6e775dc4782
MacBook Air <sghxie4@liverpool.ac.uk>
2024-03-12 18:41:19 +0000

    Add CNN_id model to models.py

--- python/models.py
+++ python/models.py
@@ -33,3 +33,35 @@ class CNN(nn.Module):
 def get_CNN():
     CNN_model = CNN()
     return CNN_model
+
+
+
+class CNN_id(nn.Module,):
+    def __init__(self):
+        super(CNN_id, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.dropout1 = nn.Dropout(0.25)
+        self.dropout2 = nn.Dropout(0.5)
+        self.fc1 = nn.Linear(64 * 32 * 6, 128)
+        self.fc2 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = nn.ReLU()(x)
+        x = nn.MaxPool2d(2)(x)
+        x = self.conv2(x)
+        x = nn.ReLU()(x)
+        x = nn.MaxPool2d(2)(x)
+        x = torch.flatten(x, 1)
+        x = self.dropout1(x)
+        x = self.fc1(x)
+        x = nn.ReLU()(x)
+        x = self.dropout2(x)
+        x = self.fc2(x)
+        output = nn.LogSoftmax(dim=1)(x)
+        return output
+
+def get_CNN_id():
+    CNN_id_model = CNN_id()
+    return CNN_id_model
