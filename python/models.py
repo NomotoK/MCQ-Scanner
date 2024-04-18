35a2180564143039e94f99ce456e1bc762e740ed
MacBook Air <sghxie4@liverpool.ac.uk>
2024-04-18 16:59:47 +0100

    Refactor file paths, update master answer CSV file, and add PDF upload functionality

--- python/models.py
+++ python/models.py
@@ -121,25 +121,227 @@ def get_EnhancedCNN():
 
 
 
-class ResNet(nn.Module):
-    def __init__(self, num_classes=6):
-        super(ResNet, self).__init__()
-        self.pre_layers = nn.Sequential(
-            nn.Conv2d(1, 3, kernel_size=3, padding=1),  # 将单通道图像转换为三通道
-            nn.Upsample(size=(224, 224), mode='bilinear', align_corners=False),  # 上采样至接近标准尺寸
-        )
-        resnet = models.resnet18(pretrained=True)
-        resnet.fc = nn.Linear(resnet.fc.in_features, num_classes)
-        
-        # 保留ResNet除了原始的第一层卷积层以外的所有层
-        self.resnet = nn.Sequential(*list(resnet.children())[1:-1], resnet.fc)
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_channels, out_channels, stride=1):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_channels != self.expansion * out_channels:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels, self.expansion * out_channels, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion * out_channels)
+            )
 
     def forward(self, x):
-        x = self.pre_layers(x)
-        x = self.resnet(x)
-        return x
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
 
-def get_resnet(num_classes=6):
-    model = ResNet(num_classes=num_classes)
+class ResNet(nn.Module):
+    def __init__(self, block, num_blocks, num_classes=6):
+        super(ResNet, self).__init__()
+        self.in_channels = 64
+
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        self.linear = nn.Linear(512 * block.expansion, num_classes)
+
+    def _make_layer(self, block, out_channels, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_channels, out_channels, stride))
+            self.in_channels = out_channels * block.expansion
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = F.avg_pool2d(out, 4)
+        out = out.view(out.size(0), -1)
+        out = self.linear(out)
+        return out
+
+
+def get_ResNet18():
+    model = ResNet(BasicBlock, [2, 2, 2, 2])
     return model
 
+
+
+
+
+
+
+
+
+class AlexNet(nn.Module):
+    def __init__(self, num_classes=6):
+        super(AlexNet, self).__init__()
+        self.features = nn.Sequential(
+            # 第一层：卷积层，使用11x11的卷积核，步长为4
+            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            # 第二层：卷积层，使用5x5的卷积核
+            nn.Conv2d(64, 192, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            # 三到五层：更多的卷积层
+            nn.Conv2d(192, 384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+        self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
+        self.classifier = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(256 * 6 * 6, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, num_classes),
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        return x
+
+
+def get_AlexNet():
+    model = AlexNet()
+    return model
+
+
+
+
+
+
+class VGG(nn.Module):
+    def __init__(self, features, num_classes=6, init_weights=True):
+        super(VGG, self).__init__()
+        self.features = features
+        self.avgpool = nn.AdaptiveAvgPool2d((7, 7))
+        self.classifier = nn.Sequential(
+            nn.Linear(512 * 7 * 7, 4096),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(4096, num_classes),
+        )
+        if init_weights:
+            self._initialize_weights()
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        return x
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.constant_(m.bias, 0)
+
+def get_VGG16():
+    model = VGG(models.vgg.make_layers(models.vgg.cfgs['D']), num_classes=6)
+    return model
+
+
+
+
+
+class ShuffleNetUnit(nn.Module):
+    def __init__(self, in_channels, out_channels, stride):
+        super(ShuffleNetUnit, self).__init__()
+        self.stride = stride
+        mid_channels = out_channels // 4
+        g = 1 if in_channels == 24 else 2
+        
+        # 1x1 group conv
+        self.conv1 = nn.Conv2d(in_channels, mid_channels, 1, groups=g, bias=False)
+        self.bn1 = nn.BatchNorm2d(mid_channels)
+        self.conv2 = nn.Conv2d(mid_channels, mid_channels, 3, stride, 1, groups=mid_channels, bias=False)
+        self.bn2 = nn.BatchNorm2d(mid_channels)
+        self.conv3 = nn.Conv2d(mid_channels, out_channels, 1, groups=g, bias=False)
+        self.bn3 = nn.BatchNorm2d(out_channels)
+
+        if stride != 1 or in_channels != out_channels:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, 1, stride=stride, bias=False),
+                nn.BatchNorm2d(out_channels)
+            )
+    
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = F.relu(self.bn2(self.conv2(out)))
+        out = self.bn3(self.conv3(out))
+        
+        res = x if self.stride == 1 and hasattr(self, 'shortcut') else self.shortcut(x)
+        out += res
+        return F.relu(out)
+
+
+class ShuffleNet(nn.Module):
+    def __init__(self, num_classes=6):
+        super(ShuffleNet, self).__init__()
+        self.conv1 = nn.Conv2d(3, 24, 3, 2, 1, bias=False)
+        self.bn1 = nn.BatchNorm2d(24)
+        self.maxpool = nn.MaxPool2d(3, 2, 1)
+        
+        self.stage2 = self._make_stage(24, 48, 3, 2)
+        self.stage3 = self._make_stage(48, 96, 7, 2)
+        self.stage4 = self._make_stage(96, 192, 3, 2)
+
+        self.fc = nn.Linear(192, num_classes)
+        
+    def _make_stage(self, in_channels, out_channels, num_units, stride):
+        stages = [ShuffleNetUnit(in_channels, out_channels, stride)]
+        for i in range(1, num_units):
+            stages.append(ShuffleNetUnit(out_channels, out_channels, 1))
+        return nn.Sequential(*stages)
+
+    def forward(self, x):
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = self.maxpool(x)
+        x = self.stage2(x)
+        x = self.stage3(x)
+        x = self.stage4(x)
+        x = F.adaptive_avg_pool2d(x, 1)
+        x = torch.flatten(x, 1)
+        x = self.fc(x)
+        return x
